a044442ab20610d43a17890ef0aa59ccaacad161
TAJ <124231329+tajmahal226@users.noreply.github.com>
2024-09-20 11:18:19 -0400

    Create script.js
    create

---
+++ script.js
@@ -0,0 +1,6 @@
+const { useState, useEffect } = React;
+const { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } = Recharts;
+
+// Paste the SalesDashboard component here
+
+ReactDOM.render(<SalesDashboard />, document.getElementById('root'));
